7e4316382bf39e37a66df7c2cd65e5da5ce5046d
yogiyiorgos <georgefil@outlook.com>
2022-08-26 20:21:16 +0200

    Product  and List reducers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store.js'
 import App from './App'
 import './index.css'
 import './bootstrap.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <App />{' '}
+  </Provider>,
+  document.getElementById('root')
+)
